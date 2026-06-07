27bd59fefca0c7d960f307815dbd5519f2a53cf5
phihash <insharp0220@gmail.com>
2026-06-07 23:32:33 +0900

    mypage

---
+++ umi/MyPage.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct MyPage: View {
+    var body: some View {
+        Text("マイページ")
+    }
+}
--- umi/NavBar.swift
+++ umi/NavBar.swift
@@ -1,15 +1,24 @@
 import SwiftUI
 
 struct NavBar: View {
+    @State private var showMyPage = false
+
     var body: some View {
         HStack {
             Spacer()
-            Circle()
-                .fill(Color.teal.opacity(0.7))
-                .frame(width: 36, height: 36)
+            Button {
+                showMyPage = true
+            } label: {
+                Circle()
+                    .fill(Color.teal.opacity(0.7))
+                    .frame(width: 36, height: 36)
+            }
         }
         .padding(.horizontal, 20)
         .padding(.top, 12)
         .padding(.bottom, 16)
+        .sheet(isPresented: $showMyPage) {
+            MyPage()
+        }
     }
 }
