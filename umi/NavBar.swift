883b2e532c8fdeb65bbfe2e80a5bcfe6b4807aaa
phihash <insharp0220@gmail.com>
2026-06-07 23:14:54 +0900

    ナビバー

---
+++ umi/NavBar.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Circle()
+                .fill(Color.teal.opacity(0.7))
+                .frame(width: 36, height: 36)
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 12)
+        .padding(.bottom, 16)
+    }
+}
---
+++ umi/umiApp.swift
@@ -0,0 +1,17 @@
+//
+//  umiApp.swift
+//  umi
+//
+//  Created by Kento Nakai on 2026/06/05.
+//
+
+import SwiftUI
+
+@main
+struct umiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
